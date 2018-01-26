3a267d6e94c15c9b4e5051a2745ae16bfb493bcd
pigxo <429404603@qq.com>
2018-01-26 18:15:44 +0800

    add some docstring

--- src/neo_python_tool/query.py
+++ src/neo_python_tool/query.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Author: Trinity Core Team
 
 MIT License
@@ -22,50 +23,44 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
-import pickle
+import pprint
+
+from sqlalchemy import create_engine, and_
+from sqlalchemy.orm import sessionmaker
+
+from neo_python_tool.model import Vout, LocalBlockCout
+import os
+
+PATH =  os.path.dirname(__file__)
+BlockDB = os.path.join(PATH,"privtnet.db")
+
+engine = create_engine('sqlite:///'+BlockDB)
+SessionBlock = sessionmaker(bind=engine)
+
+sessionblock=SessionBlock()
+
+def get_current_block_number():
+    localBlockCount = sessionblock.query(LocalBlockCout).all()
+    if localBlockCount:
+        localBlockCountInstace=localBlockCount[0]
+        print ("Current Block Number:",localBlockCountInstace.height)
+        return localBlockCountInstace.height
+    else:
+        return None
 
 
-def dic2btye(file_handler, **kwargs):
-    """
+def get_utxo_by_address(address, asset_id):
+    class_instacce=sessionblock.query(Vout).filter(and_(Vout.address==address, Vout.asset_id == asset_id)).all()
+    result=[]
+    if class_instacce:
+        for i in class_instacce:
+            result.append (i.to_json())
+        return result
 
-    :param kwargs:
-    :return:
-    """
-    return pickle.dump(kwargs, file_handler)
 
 
-def crypto_channel(file_handler, **kwargs):
-    """
-    :param kwargs:
-    :return:
-    """
-    return dic2btye(file_handler, **kwargs)
 
-def uncryto_channel(file_handler):
-    """
 
-    :param file_handler:
-    :return:
-    """
-    return byte2dic(file_handler)
 
-def byte2dic(file_hander):
-    """
 
-    :param file_hander:
-    :return:
-    """
-    return pickle_load(file_hander)
 
-def pickle_load(file):
-    """
-
-    :param file:
-    :return:
-    """
-    pickles = []
-    while True:
-        try:
-            pickles.append(pickle.load(file))
-        except EOFError:
-            return pickles
