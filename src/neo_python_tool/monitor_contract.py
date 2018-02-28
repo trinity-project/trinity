6f4bdf6a55d580b3d11005bd60a45119b25d7471
xiaohuta <xiaohuta@gmail.com>
2018-03-01 02:09:33 +0800

    MODIFY: change to adapt contract monitor functionality

--- src/neo_python_tool/monitor_contract.py
+++ src/neo_python_tool/monitor_contract.py
@@ -21,121 +21,260 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
+import os
+import gevent
+import requests
+from gevent.queue import Queue
+from gevent.threading import Lock
+from gevent.event import Event
+from logzero import logger
+from sqlalchemy import or_
+from datetime import datetime
 
 from configure import Configure
-import threading
-from time import sleep
-from logzero import logger
+from channel_manager.state import ChannelDatabase, Session
+from channel_manager.channel import State
 
 
-from twisted.internet import reactor, task
+class NodeBApi(object):
+    def __init__(self):
+        # NODE-B URL
+        nodeb_net_port = os.getenv('NODEB_NET_PORT')
+        if nodeb_net_port:
+            self.nodeb_api_uri = Configure["NodeBNet"].replace('21332', str(nodeb_net_port))
+        else:
+            self.nodeb_api_uri = Configure["NodeBNet"]
+        self.nodeb_api_uri += '/api'
 
-from neo.contrib.smartcontract import SmartContract
-from neo.Network.NodeLeader import NodeLeader
-from neo.Core.Blockchain import Blockchain
-from neo.Implementations.Blockchains.LevelDB.LevelDBBlockchain import LevelDBBlockchain
-from neo.Settings import settings
+    def get_channel_state_from_nodeb(self, data=None, json=None):
+        """
 
-from channel_manager.channel import get_channelnames_via_address, Channel, State
-from channel_manager.manager import close_channel
-from utils.channel import split_channel_name
-from crypto.Cryptography.Helper import hash_to_wallet_address,bytes_to_hex_string, hex2interger
+        :return:
+        """
+        url = self.nodeb_api_uri + '/confirmTx'
+
+        if data or json:
+            return requests.post(url, data=data, json=json).json()
+
+        return None
 
 
+class MonitorDaemon(object):
+    """
+    Monitor Thread set to handle the transaction states from the trinity networks.
+    """
+    def __init__(self):
+        self.register_lock = Lock()
+        self.spawn_signal = Event()
+        self.__t_reg = {}
 
-smart_contract = SmartContract(Configure["TNC"].replace("0x",""))
-ContractAddr = Configure["ContractAddr"]
-DepositIN =[]
-DepositOut = []
+        # set the worker timeout (unit: second)
+        self.main_timeout = 1
+        self.worker_timeout = 1
 
-# Register an event handler for Runtime.Notify events of the smart contract.
-@smart_contract.on_notify
-def sc_notify(event):
-    logger.info("SmartContract Runtime.Notify event: %s", event)
+        # record the mapping relationship between tx_id and channel_name
+        self.tx_id_channel_map = {}
 
-    if not len(event.event_payload):
+        #
+        self.nodeb_api = NodeBApi()
+
+    def worker(self, callback, *args, **kwargs):
+        """
+        Worker to handler the each response from the NODE-B
+        :param callback: The real handler called by worker.
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        transport = self.__register_transport(callback.__name__)
+        if not transport:
+            logger.info('No monitor worker thread is created. Channel state will never be updated.')
+            return
+
+        try:
+            while True:
+                task = transport.get(timeout=self.worker_timeout)
+                if task:
+                    callback(task)
+        except Exception as e:
+            # Never run here.
+            logger.error('Exception occurred in the worker thread. exceptions: {}'.format(e))
+
         return
 
-    logger.info("- payload part 1: %s", event.event_payload[0].decode("utf-8"))
-    tx_type = event.event_payload[0]
-    sender = hash_to_wallet_address(event.event_payload[1])
-    receiver = hash_to_wallet_address(event.event_payload[2])
-    value = hex2interger(bytes_to_hex_string(event.event_payload[3]))
-    if not tx_type.decode() == "transfer":
+    def daemon_monitor_timer_event(self, interval = 10):
+        self.spawn_signal.clear()
+        self.spawn_signal.wait(5)
+        if self.spawn_signal.is_set():
+            logger.info('Start the Monitor timer event.')
+        else:
+            logger.info('Why no transport is registered????')
+            return
+
+        # timer event to handle the response from the NODE-B
+        try:
+            while True:
+                start_time = datetime.now()
+                self.confirm_channel_state(self.all_opening_channels)
+                self.confirm_channel_state(self.all_settling_channels, 'settled')
+
+                # calculate the total seconds
+                total_seconds = (datetime.now()-start_time).total_seconds()
+                if interval > total_seconds:
+                    gevent.sleep(interval - total_seconds)
+                else:
+                    # sleep 500 ms
+                    gevent.sleep(0.5)
+
+        except Exception as e:
+            logger.error('exception info: {}'.format(e))
+
+        # clear the spawn signal. should never run here.
+        self.spawn_signal.clear()
+
+        # need delete the resources here ??????
+
         return
-    if receiver == ContractAddr:
-        depoist_in(address=sender,value=value)
-    elif sender == ContractAddr:
-        depoist_out(address=receiver, value=value)
-    print("sc_notification")
 
+    def confirm_channel_state(self, channel_set, expected_state = 'open', count_per_second = 100):
+        """
 
-def depoist_in(address, value):
-    print("depost_in", address)
-    channels = get_channelnames_via_address(address)
-    print(channels)
-    success_channel = []
-    for channel in channels:
-        print(channel.channel_name)
-        sender, receiver = split_channel_name(channel.channel_name)
-        ch = Channel(sender, receiver)
-        #if address == sender and value == ch.sender_deposit_cache:
-        if address == sender:
-            ch.set_channel_open()
-            success_channel.append(channel.channel_name)
-        #elif address == receiver and value == ch.receiver_deposit_cache:
-        elif address == receiver:
-            ch.set_channel_open()
-            success_channel.append(channel.channel_name)
+        :param channel_set:
+        :param count_per_second:
+        :return:
+        """
+        if not channel_set:
+            return
+
+        # parse the all channels from the channel sets
+        total_channels = len(channel_set)
+        send_loop = total_channels / count_per_second
+        left_chan = total_channels % count_per_second
+
+        # handle the left channels
+        if 0 != left_chan:
+            tx_id_list = list()
+            for ch in channel_set[send_loop*self.count_per_second::]:
+                tx_id_list.append(ch.tx_id)
+                self.all_channels.update({ch.tx_id: ch.channel_name})
+
+            # post data to the NODE-B
+            response = self.nodeb_api.get_channel_state_from_nodeb(json={'tx_id': list(set(tx_id_list))})
+            self.add_task(task={expected_state.lower():response})
+
+        # handle the request wrapped by self.count_per_second
+        for loop_index in range(send_loop):
+            start_index = loop_index * count_per_second
+            tx_id_list = list()
+            for ch in channel_set[start_index::start_index+count_per_second]:
+                tx_id_list.append(ch.tx_id)
+                self.tx_id_channel_map.update({ch.tx_id: ch.channel_name})
+
+            response = self.nodeb_api.get_channel_state_from_nodeb(json={'tx_id': list(set(tx_id_list))})
+            self.add_task(task={expected_state.lower():response})
+
+        return
+
+    def update_channel(self, task):
+        if not task:
+            return
+
+        if task.get('open'):
+            tx_id_list = task.get('open')
+            expected_state = State.OPEN
+        elif task.get('settled'):
+            tx_id_list = task.get('settled')
+            expected_state = State.SETTLED
         else:
-            continue
-    print("depost in",success_channel)
+            logger.info('unknown expected state of channels')
+            return
+
+        for tx_id_item in tx_id_list:
+            for tx_id, status in tx_id_item.keys():
+                if status is True:
+                    channel_set = Session.query(ChannelDatabase).filter(
+                        ChannelDatabase.channel_name == self.tx_id_channel_map[tx_id]).first()
+                    if channel_set:
+                        channel_set.state = expected_state
+
+        # save the database
+        Session.commit()
+
+    def add_task(self, task):
+        tid_list = self.__t_reg.keys()
+        transport = self.__t_reg[tid_list[0]]
+        transport.add_task(task)
+
+    @property
+    def all_opening_channels(self):
+        # get all of the opening channels
+        channel_set = Session.query(ChannelDatabase).filter(or_(ChannelDatabase.state == State.OPENING,
+                                                                ChannelDatabase.state == State.UPDATING)).all()
+        return channel_set if channel_set else []
+
+    @property
+    def all_settling_channels(self):
+        # get all of the opening channels
+        channel_set = Session.query(ChannelDatabase).filter(ChannelDatabase.state == State.SETTLING).all()
+        return channel_set if channel_set else []
 
 
+    '''Private function'''
+    class Transport(object):
+        def __init__(self, tid, name):
+            self.__tid  = tid
+            self.__name = name
+            self.q_maxsize = 100
+            self.__task_queue = Queue(maxsize=self.q_maxsize)
 
-def depoist_out(address,value):
-    print("deposit_out", address)
-    channels = get_channelnames_via_address(address)
-    success_channel = []
-    for channel in channels:
+        def add_task(self, task):
+            if self.__task_queue.qsize() < self.q_maxsize:
+                self.__task_queue.put_nowait(task)
+                return True
+            else:
+                # queue is full
+                logger.debug('The task queue is full')
+                return False
 
-        if channel.state == State.SETTLING.value:
-            sender, receiver = split_channel_name(channel.channel_name)
-            ch = Channel(sender, receiver)
-            ch.close()
-            success_channel.append(channel.channel_name)
-        else:
-            continue
-        print("depost out", success_channel)
+        def get(self, timeout = None):
+            task = None
+            try:
+                task = self.__task_queue.get(block=True, timeout= timeout)
+            except Exception as e:
+                if not self.__task_queue.empty():
+                    logger.error('Transport Thread still in processing data')
+            finally:
+                return task
 
+        @property
+        def name(self):
+            return str(self.__name)
 
-def custom_background_code():
+    def __register_transport(self, name):
+        if not name:
+            logger.error( 'Failed to register {}'.format(name) )
+            return None
 
-    while True:
-        logger.info("Block %s / %s", str(Blockchain.Default().Height), str(Blockchain.Default().HeaderHeight))
-        sleep(15)
+        self.register_lock.acquire()
+        tid = len(self.__t_reg.keys())
+        t_name = '{}-{}'.format(name, tid)
+
+        # if name has already existed, don't change
+        transport = self.Transport(tid, t_name)
+        self.__t_reg.update({}.fromkeys([tid], transport))
+        self.register_lock.release()
+        self.spawn_signal.set()
+
+        return transport
 
 
 def main():
-    # Setup the blockchain
-    blockchain = LevelDBBlockchain(settings.LEVELDB_PATH)
-    Blockchain.RegisterBlockchain(blockchain)
-    dbloop = task.LoopingCall(Blockchain.Default().PersistBlocks)
-    dbloop.start(.1)
-    NodeLeader.Instance().Start()
+    monitor = MonitorDaemon()
+    gevent.joinall([
+        gevent.spawn(monitor.daemon_monitor_timer_event, interval=0.5),
+        gevent.spawn(monitor.worker, callback = monitor.update_channel)
+    ])
 
-    # Disable smart contract events for external smart contracts
-    settings.set_log_smart_contract_events(False)
 
-    # Start a thread with custom code
-    d = threading.Thread(target=custom_background_code)
-    d.setDaemon(True)
-    d.start()
-
-    # Run all the things (blocking call)
-    logger.info("Everything setup and running. Waiting for events...")
-    reactor.run()
-    logger.info("Shutting down.")
-
-if __name__ == "__main__":
+if "__main__" == __name__:
     main()
