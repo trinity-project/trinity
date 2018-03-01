efbb5702a0d9c319702b0f53a31b8a805130c57e
pigxo <429404603@qq.com>
2018-03-02 00:18:30 +0800

    update deposit

--- src/channel_manager/blockchain.py
+++ src/channel_manager/blockchain.py
@@ -41,7 +41,7 @@ def construct_raw_tx(tx_data, signature, publickey):
     return ns.construct_raw_tx(tx_data, signature, publickey)
 
 def deposit_transaction(asset_type, from_addr, to_addr, count):
-    tx_data= ns.construct_tx(from_addr, to_addr, count, Configure["AssetList"].get(asset_type.upper()))
+    tx_data= ns.construct_tx(from_addr, to_addr, int(count), Configure["AssetList"].get(asset_type.upper()))
     if tx_data.get("txData") and tx_data.get("txid"):
         return tx_data.get("txData"), tx_data.get("txid"), True
     try:
