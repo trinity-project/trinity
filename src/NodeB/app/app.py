856260b58388bef87959003d2dbc1a43ad3f76b1
aa <1730105653@qq.com>
2018-03-01 10:10:42 +0800

    modify

--- src/NodeB/app/app.py
+++ src/NodeB/app/app.py
@@ -34,7 +34,8 @@ pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 cors = CORS(app, support_credentials=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s/%s' %(MYSQLDATABASE["user"],MYSQLDATABASE["passwd"],MYSQLDATABASE["host"],MYSQLDATABASE["db"])
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s/%s' %(MYSQLDATABASE["user"],MYSQLDATABASE["passwd"],MYSQLDATABASE["host"],MYSQLDATABASE["db"])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
 db = SQLAlchemy(app)
 jsonrpc = JSONRPC(app, "/")
--- src/NodeB/config.py
+++ src/NodeB/config.py
@@ -1,7 +1,7 @@
 
 
 MYSQLDATABASE={
-    "host":"192.168.203.64",
+    "host":"localhost",
     "user":"root",
     "passwd":"root",
     "db":"test"
@@ -10,9 +10,9 @@ MYSQLDATABASE={
 
 NEO_ASSETID="0xc56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b"
 GAS_ASSETID="0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7"
-CONTRACTHASH="0c34a8fd0109df360c7cf7ca454404901db77f5e"
+CONTRACTHASH="849d095d07950b9e56d0c895ec48ec5100cfdff1"
 
-NEOCLIURL="http://127.0.0.1:10332"
+NEOCLIURL="http://localhost:20332"
 
 headers = {
     "Content-Type": "application/json"
