9ecf5eac44b0f0df7dd6a3b9bec62f26bc355c5a
pigxo <429404603@qq.com>
2018-04-02 17:40:09 +0800

    Add debug info

--- wallet/utils.py
+++ wallet/utils.py
@@ -32,8 +32,9 @@ def pubkey_to_address(publickey: str):
 
 
 def sign(wallet, context):
-    keypair = wallet.LoadKeyPairs().values()
-    keypair = [i for i in keypair][0]
+    keypairs = wallet.LoadKeyPairs().values()
+    keypair = [i for i in keypairs][0]
+    print("Debug Keypair",keypair, keypairs, keypair.PrivateKey)
     res = binascii.hexlify(Crypto.Sign(message=context, private_key=bytes(keypair.PrivateKey))).decode()
 
 
