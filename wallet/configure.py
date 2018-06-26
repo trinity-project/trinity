eada1e54da8f3f0cc7a66eab1eeaf9c4ca19b6ee
wei wu <429404603@qq.com>
2018-06-26 14:22:23 +0800

    change verson to v0.2.7

--- wallet/configure.py
+++ wallet/configure.py
@@ -56,7 +56,7 @@ Configure = {
     },
     "DataBase":{"url": "http://localhost:20554"
     },
-    "Version":"v0.2.6",
+    "Version":"v0.2.7",
     "Magic":{
         "Block":1953787457,
         "Trinity":19990331
